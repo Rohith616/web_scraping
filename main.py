75e37d93716a356cbf814895116322938a5133aa
Rohith <90886898+Rohith616@users.noreply.github.com>
2022-08-02 20:34:34 +0000

    scraped without js

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.moe.gov.sg/private-education/private-schools'
+
+res = requests.get(url)
+print(res.status_code)
+doc = BeautifulSoup(res.text,'html.parser')
+print(doc.title.text)
+print(doc)
